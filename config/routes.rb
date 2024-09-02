3e962c8d1d7aa14ac4a91834ac2d56d515e88205
EC2 Default User <ec2-user@ip-172-31-18-173.ap-northeast-1.compute.internal>
2024-09-02 10:07:05 +0000

    ダウングレードのために一時的にpush

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
+# Rails.application.routes.draw do
+#   root "home#top"
+#   delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+#   resources :lists
+
+#   get "up" => "rails/health#show", as: :rails_health_check
+#   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+#   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+# end
+
 Rails.application.routes.draw do
-  root "home#top"
-
-  resources :lists
-
-  get "up" => "rails/health#show", as: :rails_health_check
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get 'lists/new'
+  get 'top' => 'homes#top'
+  post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
+  get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
 end
